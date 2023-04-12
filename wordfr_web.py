32ad4c54d77550e6d349549c9baa8b5596162740
Marko-air <m.niinimaki@myfriendapp.com>
2023-04-12 11:32:55 +0700

    a web form

---
+++ wordfr_web.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return """
+<form action="getform" method="post">
+    <textarea name="text" rows="20" cols="100"></textarea>
+    <br/>
+    <input type="submit">
+</form>
+           """
+
+@app.route('/getform', methods=['POST'])
+def getform():
+    return 'You entered: {}'.format(request.form['text'])
+
+
+if __name__ == '__main__':
+    app.run()
